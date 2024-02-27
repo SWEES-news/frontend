e003b64e354d96d0a48ed86c0f62507dfb90a023
Ethan Bootehsaz <ethandb2024@gmail.com>
2024-02-26 16:09:35 -0800

    adding user correctly shows userId

--- src/Components/Users/AddUser.jsx
+++ src/Components/Users/AddUser.jsx
@@ -6,11 +6,11 @@ const AddUser = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [submissionResponse, setSubmissionResponse] = useState(null); // New state for the submission response
+    const [submissionResponse, setSubmissionResponse] = useState(null);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
+
         const userData = {
             Username: name,
             Email: email,
@@ -19,21 +19,23 @@ const AddUser = () => {
 
         try {
             const response = await axios.post('/user', userData);
-            console.log('User added:', response.data);
-            setSubmissionResponse({ message: "User added successfully!", submission_id: response.data.id }); // Set success message
+            console.log('Response as a dictionary:', JSON.stringify(response.data, null, 2)); // Print the response as a JSON string
+            const userId = response.data.UserID;
+            console.log("User added successfully!", userId);
+            setSubmissionResponse({ message: "User added successfully!", userId: userId });
             setName('');
             setEmail('');
             setPassword('');
         } catch (error) {
             console.error('Error adding user:', error);
-            setSubmissionResponse({ message: "Error adding user.", submission_id: "" }); // Set error message
+            setSubmissionResponse({ message: "Error adding user.", userId: "" });
         }
     };
 
     return (
         <div className="article-submission-container">
             <form onSubmit={handleSubmit} className="article-submission-form">
-            <label>
+                <label>
                     Name:
                     <input 
                         type="text" 
@@ -65,7 +67,7 @@ const AddUser = () => {
             {submissionResponse && (
                 <div className="article-submission-response">
                     <p>{submissionResponse.message}</p>
-                    <p>Submission ID: {submissionResponse.submission_id}</p>
+                    <p>User ID: {submissionResponse.userId}</p>
                 </div>
             )}
         </div>
