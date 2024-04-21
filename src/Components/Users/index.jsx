cd95817e266c1c993169321cb4b56d0e3b633491
Ethan Bootehsaz <ethandb2024@gmail.com>
2024-04-21 12:40:04 -0700

    Added to index.jsx for new registration control flow

--- src/Components/Users/index.jsx
+++ src/Components/Users/index.jsx
@@ -2,5 +2,8 @@ import RegisterUser from "./register";
 import LoginUser from "./login";
 import LogoutUser from "./logout";
 import UserProvider from "./UserContext";
+import StartRegistration from './StartRegistration';
+import VerifyEmail from './VerifyEmail';
+import RegisterDetails from './RegisterDetails'; 
 
-export {RegisterUser, LoginUser, LogoutUser, UserProvider};
+export {RegisterUser, LoginUser, LogoutUser, UserProvider, StartRegistration, VerifyEmail, RegisterDetails};
